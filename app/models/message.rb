86cecbb2ba9969bd3bde146b27b6dc9fbf127d93
Stan Carver <stan@a1webconsulting.com>
2013-12-11 07:28:22 -0600

    removed allow blank from email validator

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,5 +7,5 @@ class Message
   attribute :body
   
   validates_presence_of :name, :email, :subject, :body
-  validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => true
+  validates :email, :format => { :with => %r{.+@.+\..+} } # , :allow_blank => true
 end
